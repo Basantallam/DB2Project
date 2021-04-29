190c791647a1ac30bb98c4de5af8bb512c57a71a
aya <aya.elgamal14@gmail.com>
2021-04-29 16:25:16 +0200

    Overall Interface

--- src/main/java/DBApp.java
+++ src/main/java/DBApp.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Set;
 
 public class DBApp implements DBAppInterface {
     HashSet<String> DB;
@@ -18,6 +18,7 @@ public class DBApp implements DBAppInterface {
 
     private int getCapacity() {
         //todo from AppDB.config
+        return 250;
     }
 
     private void addtoDB() {
@@ -27,8 +28,10 @@ public class DBApp implements DBAppInterface {
     @Override
     public void createTable(String tableName, String clusteringKey, Hashtable<String, String> colNameType, Hashtable<String, String> colNameMin, Hashtable<String, String> colNameMax) throws DBAppException {
         if (!DB.contains(tableName)) {
+            DB.add(tableName);
             try {
-                DB.put(tableName, new Table(tableName, clusteringKey, colNameType, colNameMin, colNameMax));
+                Table t = new Table( tableName,  clusteringKey,  colNameType, colNameMin, colNameMax);
+                serialize(tableName,t);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -44,18 +47,51 @@ public class DBApp implements DBAppInterface {
     @Override
     public void insertIntoTable(String tableName, Hashtable<String, Object> colNameValue) throws DBAppException {
         if (DB.contains(tableName)) {
-            Table table = DB.get(tableName);
-            table.insert(colNameValue);  //TODO
+
+
+           String pk= checkinMeta( tableName,  colNameValue);
+            Table table= (Table) deserialize(tableName);
+            table.insert(colNameValue,pk);
+            serialize(tableName,table);
 
 
         } else throw new DBAppException("Table does not exist in Database");
     }
 
+    private String checkinMeta(String tableName, Hashtable<String, Object> colNameValue) throws DBAppException {
+        //todo retrieve from meta data to chdeck and save pk
+//        if (colNameValue.get((Object) pk) == null)
+//            throw new DBAppException(); // TODO
+//        else {
+//            // Set<String> original = this.htblColNameType.keySet();
+//            Set<String> input = colNameValue.keySet();
+//            // todo all checks will be in AppDB
+//            for (String key : input) {
+//                if (!(this.htblColNameType.containsKey(key)))
+//
+//                    throw new DBAppException("column name doesn't exist");// TODO + do we break here?
+//
+//                if (!(this.htblColNameType.get(key).equals((colNameValue.get(key).getClass()).toString())))
+//                    // checking correct data types
+//                    throw new DBAppException("incorrect datatype");// TODO + do we break here?
+//                if ((this.htblColNameMax.get(key)).compareTo((colNameValue.get(key).getClass()).toString()) < 0)
+//                    throw new DBAppException("value entered is above max");// TODO + do we break here?
+//                if ((this.htblColNameMin.get(key)).compareTo((colNameValue.get(key).getClass()).toString()) > 0)
+//                    throw new DBAppException("value entered is below min");// TODO + do we break here?
+//
+//            }
+//        }
+
+        return null;
+    }
+
     @Override
     public void updateTable(String tableName, String clusteringKeyValue, Hashtable<String, Object> columnNameValue) throws DBAppException {
         if (DB.contains(tableName)) {
-            Table table = DB.get(tableName);
+            Table table = (Table)deserialize(tableName);
+            checkinMeta(tableName,columnNameValue);
             table.update(clusteringKeyValue, columnNameValue);
+            serialize(tableName,table);
         } else throw new DBAppException("Table does not exist in Database");
 
     }
@@ -63,8 +99,9 @@ public class DBApp implements DBAppInterface {
     @Override
     public void deleteFromTable(String tableName, Hashtable<String, Object> columnNameValue) throws DBAppException {
         if (DB.contains(tableName)) {
-            Table table = DB.get(tableName);
+            Table table = (Table)deserialize(tableName);
             table.delete(columnNameValue);
+            serialize(tableName,table);
 
         } else throw new DBAppException("Table does not exist in Database");
 
@@ -95,22 +132,23 @@ public class DBApp implements DBAppInterface {
             if (!(DB.contains(sqlTerms[i].strTableName)))
                 throw new DBAppException("Table does not exist in Database");
             else {
-                table = DB.get(sqlTerms[i].strTableName); //TODO hashset instead of hashtable table in serialized files
-                if (!(table.htblColNameType.containsKey(sqlTerms[i].strColumnName)))
-                    throw new DBAppException("Column" + sqlTerms[i].strColumnName + "does not exist in Table: " + sqlTerms[i].strTableName);
-                else {
-                    column = sqlTerms[i].strColumnName;
-                    valueType = (sqlTerms[i].objValue.getClass()).toString();
-                    if (!(table.htblColNameType.get(column).equals(valueType)))
-                        throw new DBAppException("Value has an incorrect data type");
-                    else {
-                        //todo
-
-                    }
-                }
+                table = (Table)deserialize(sqlTerms[i].strTableName); //TODO hashset instead of hashtable table in serialized files
+//                if (!(table.htblColNameType.containsKey(sqlTerms[i].strColumnName)))
+//                    throw new DBAppException("Column" + sqlTerms[i].strColumnName + "does not exist in Table: " + sqlTerms[i].strTableName);
+//                else {
+//                    column = sqlTerms[i].strColumnName;
+//                    valueType = (sqlTerms[i].objValue.getClass()).toString();
+//                    if (!(table.htblColNameType.get(column).equals(valueType)))
+//                        throw new DBAppException("Value has an incorrect data type");
+//                    else {
+//                        //todo
+//
+//                    }
+//
+//                }  //todo checking in meta
             }
         }
-        Iterator pagesItr = (table.Pages).iterator();
+        Iterator pagesItr = (table.table).iterator();
         Iterator recs = null;
         Iterator res = null;
         Page currPage;
--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,30 +1,27 @@
-import org.hamcrest.core.StringContains;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.*;
-import java.util.Map.Entry;
 
-public class Table implements Serializable {
+public class Table implements Serializable  {
 	String tableName;
-//	transient String pk;
+//	String pk;
 //	transient Hashtable<String, String> htblColNameType;
 //	transient Hashtable<String, String> htblColNameMin;
 //	transient Hashtable<String, String> htblColNameMax;
-	Vector<Page> Pages; //todo page ranges and ids
-	transient Vector<Pair> range; // every element corresponds to a page
+	Vector<tuple4> table; //todo page ranges and ids
+
 
 	public Table(String strTableName, String strClusteringKeyColumn, Hashtable<String, String> htblColNameType,
 				 Hashtable<String, String> htblColNameMin, Hashtable<String, String> htblColNameMax)
 			throws DBAppException, IOException {
 		tableName = strTableName;
-		this.Pages = new Vector<Page>();
-		this.htblColNameType = htblColNameType;
-		this.htblColNameMin = htblColNameMin;
-		this.htblColNameMax = htblColNameMax;
-		pk = strClusteringKeyColumn;
-		range = new Vector<Pair>();
+		this.table = new Vector<tuple4>();
+//		this.htblColNameType = htblColNameType;
+//		this.htblColNameMin = htblColNameMin;
+//		this.htblColNameMax = htblColNameMax;
+//		pk = strClusteringKeyColumn;
+//		range = new Vector<Pair>();
 		Set<String> keys = htblColNameType.keySet();
 
 		if (strClusteringKeyColumn.equals("")) {
@@ -39,92 +36,68 @@ public class Table implements Serializable {
 				throw new DBAppException("not a valid datatype");
 		}
 
-		updateMetadata();
+		updateMetadata( strClusteringKeyColumn,  htblColNameType,
+				 htblColNameMin,  htblColNameMax);
 
 		//TODO convert types into the datatypes
 
 	}
 
-	public void insert(Hashtable<String, Object> colNameValue) throws DBAppException {
+	public void insert(Hashtable<String, Object> colNameValue, String pk) throws DBAppException {
 		// TODO
 		// if(!(colNameValue.containsKey((Object)this.pk)))
-		if (colNameValue.get((Object) this.pk) == null)
-			throw new DBAppException(); // TODO
-		else {
-			// Set<String> original = this.htblColNameType.keySet();
-			Set<String> input = colNameValue.keySet();
-			// todo all checks will be in AppDB
-			for (String key : input) {
-				if (!(this.htblColNameType.containsKey(key)))
 
-					throw new DBAppException("column name doesn't exist");// TODO + do we break here?
-
-				if (!(this.htblColNameType.get(key).equals((colNameValue.get(key).getClass()).toString())))
-					// checking correct data types
-					throw new DBAppException("incorrect datatype");// TODO + do we break here?
-				if ((this.htblColNameMax.get(key)).compareTo((colNameValue.get(key).getClass()).toString()) < 0)
-					throw new DBAppException("value entered is above max");// TODO + do we break here?
-				if ((this.htblColNameMin.get(key)).compareTo((colNameValue.get(key).getClass()).toString()) > 0)
-					throw new DBAppException("value entered is below min");// TODO + do we break here?
-
-				if (Pages.isEmpty()) { // will not do binary search and will insert directly
-					Page firstpage = new Page();
-					firstpage.records.add(colNameValue);
+				if (table.isEmpty()) { // will not do binary search and will insert directly
+					tuple4 firstpage = new tuple4(0,new Page(0),0,0);
+					firstpage.page.insert(colNameValue);
 
 					Object firstpk = colNameValue.get(pk);
 					firstpage.max = firstpk;
 					firstpage.min = firstpk;
-					Pages.add(firstpage);
 
-					Pair firstpair = new Pair(firstpk, firstpk);
-					range.add(firstpair);
+					table.add(firstpage);
+					DBApp.serialize(tableName+"_0",firstpage.page);
 				} else {
 					//TODO binary search for page
 
-					Page foundpage = BinarySearch(colNameValue.get(pk));
+					Page foundpage = BinarySearch(colNameValue.get(pk)); //todo deserialize and return page
 					foundpage.insert(colNameValue);// TODO
+					DBApp.serialize(tableName+"_0",foundpage.id);
 				}
 
 				//TODO now we search for correct location then we insert
 				// shift records if necessary
 				// create a new page if necessary and if you'll create new page create a new
 				// range too
-			}
-		}
+
+
 	}
 
 	public void update(String clusteringKeyValue, Hashtable<String, Object> columnNameValue) throws DBAppException {
 		//TODo update range
-		Page foundpage = BinarySearch(columnNameValue.get(pk)); // TODO currently returns null
+		Page foundpage = BinarySearch(columnNameValue.get(clusteringKeyValue)); // TODO currently returns null
 		foundpage.update(clusteringKeyValue, columnNameValue);// TODO
 	}
 
-	public void delete(Hashtable<String, Object> columnNameValue) throws DBAppException {// TODO
-
-		Page foundpage = BinarySearch(columnNameValue.get(pk)); // TODO currently returns null
-		foundpage.delete(columnNameValue);
-
-		//TODO delete entire page if last record is deleted in table use isEmpty()
-		// delete it in range vector too
-		if (foundpage.isEmpty()) {
-			int idx = Pages.indexOf(foundpage);
-			Pages.remove(idx);
-			range.remove(idx);
+	public void delete(Hashtable<String, Object> columnNameValue) throws DBAppException {
+		// todo access every page todelete records
+		for( tuple4 t  : table){
+			 t.page.delete(columnNameValue);
+			//TODO delete entire page if last record is deleted in table use isEmpty()
+			// delete it in range vector too
+			if (t.page.isEmpty()) {
+				int idx = table.indexOf(t.page);
+				table.remove(idx);
+				//todo delete its binary file
+			}
 		}
-	}
 
-	public static class Pair {
-		Object max;
-		Object min;
-
-		public Pair(Object ma, Object mi) {
-			max = ma;
-			min = mi;
-		}
 
 	}
 
-	public void updateMetadata() throws IOException {
+
+
+	public void updateMetadata(String pk, Hashtable<String, String> htblColNameType, Hashtable<String, String> htblColNameMin, Hashtable<String, String> htblColNameMax) throws IOException {
 
 		String path = "src\\main\\resources\\metadata.csv";
 		FileWriter fw = new FileWriter(path);
@@ -152,78 +125,71 @@ public class Table implements Serializable {
 
 	public Page BinarySearch(Object searchkey) {
 		// TODO
-		int hi = Pages.size(); // idx
+		int hi = table.size(); // idx
 		int lo = 0;// idx
 
-		String pktype = htblColNameType.get(pk); //todo searchkey of which type instanceof
 
-		if (pktype.equals("java.lang.Integer"))
-			return BinarySearchInt((Integer) searchkey, hi, lo);
+		if (searchkey instanceof  Integer)return BinarySearchInt((Integer) searchkey, hi, lo);
+		else if(searchkey instanceof Double) return BinarySearchDouble((Double) searchkey, hi, lo);
+		else  if (searchkey instanceof Date) return BinarySearchDate((Date) searchkey, hi, lo);
+		else   return BinarySearchString((String) searchkey, hi, lo);
 
-		else if (pktype.equals("java.lang.Double"))
-			return BinarySearchDouble((Double) searchkey, hi, lo);
 
-		else if (pktype.equals("java.lang.Date"))
-			return BinarySearchDate((Date) searchkey, hi, lo);
 
-		else if (pktype.equals("java.lang.String"))
-			return BinarySearchString((String) searchkey, hi, lo);
-
-		return null; // shouldn't ever reach this
 	}
 
 	public Page BinarySearchInt(Integer searchkey, int hi, int lo) {
 		int mid = (hi + lo) / 2;
 
-		if (((Integer) range.get(mid).max) < searchkey)
+		if (((Integer) table.get(mid).max) < searchkey)
 			return BinarySearchInt(searchkey, hi, mid);
 
-		else if (((Integer) range.get(mid).min) > searchkey)
+		else if (((Integer) table.get(mid).min) > searchkey)
 			return BinarySearchInt(searchkey, mid, lo);
 
 		else
-			return Pages.get(mid);
+			return table.get(mid).page;
 	}
 
 	public Page BinarySearchDouble(Double searchkey, int hi, int lo) {
 		int mid = (hi + lo) / 2;
 
-		if (((Double) range.get(mid).max) < searchkey)
+		if (((Double) table.get(mid).max) < searchkey)
 			return BinarySearchDouble(searchkey, hi, mid);
 
-		else if (((Double) range.get(mid).min) > searchkey)
+		else if (((Double) table.get(mid).min) > searchkey)
 			return BinarySearchDouble(searchkey, mid, lo);
 
 		else
-			return Pages.get(mid);
+			return table.get(mid).page;
 	}
 
 	public Page BinarySearchString(String searchkey, int hi, int lo) {
 
 		int mid = (hi + lo) / 2;
 
-		if (((String) range.get(mid).max).compareTo(searchkey) < 0)
+		if (((String) table.get(mid).max).compareTo(searchkey) < 0)
 			return BinarySearchString(searchkey, hi, mid);
 
-		else if (((String) range.get(mid).min).compareTo(searchkey) > 0)
+		else if (((String) table.get(mid).min).compareTo(searchkey) > 0)
 			return BinarySearchString(searchkey, mid, lo);
 
 		else
-			return Pages.get(mid);
+			return table.get(mid).page;
 	}
 
 	public Page BinarySearchDate(Date searchkey, int hi, int lo) {
 
 		int mid = (hi + lo) / 2;
 
-		if (((Date) range.get(mid).max).compareTo(searchkey) < 0)
+		if (((Date) table.get(mid).max).compareTo(searchkey) < 0)
 			return BinarySearchDate(searchkey, hi, mid);
 
-		else if (((Date) range.get(mid).min).compareTo(searchkey) > 0)
+		else if (((Date) table.get(mid).min).compareTo(searchkey) > 0)
 			return BinarySearchDate(searchkey, mid, lo);
 
 		else
-			return Pages.get(mid);
+			return table.get(mid).page;
 	}
 
 	public static void main(String args[]) throws DBAppException, IOException {
@@ -249,4 +215,17 @@ public class Table implements Serializable {
 
 	}
 
+	private class tuple4 {
+		double id;
+		Page page;
+		Object min;
+		Object max;
+
+		public tuple4(double id, Page page, Object min, Object max) {
+			this.id = id;
+			this.page = page;
+			this.max = max;
+			this.min = min;
+		}
+	}
 }
