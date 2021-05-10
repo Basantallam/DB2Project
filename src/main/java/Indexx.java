261f95ccde018df90279cf9b322b960b6b4fe04e
BASANT <basant_allam@hotmail.com>
2021-05-10 22:01:40 +0200

    deepClone()

--- src/main/java/Indexx.java
+++ src/main/java/Indexx.java
@@ -5,35 +5,48 @@ public class Indexx implements Serializable {
 	String[] columnNames;
 	Object[] buckets;
 
-	public static void main(String[] args) {
-		String[] stringarr= {"boo", "bar","foo"}; // n=3
-		Indexx idx=new Indexx(stringarr);
-		
-		System.out.println((Arrays.deepToString(idx.buckets)));//3d
-		System.out.println(Arrays.deepToString((Object[]) idx.buckets[0]));//2d
-		System.out.println(Arrays.toString((Object[])(((Object[]) idx.buckets[0])[0]))); //1d
-	}
 	public Indexx(String[] columnNames) {
 		this.columnNames = columnNames;
 		int n = columnNames.length;
 
 		Object[] temp = new Object[10];
 		Object[] temp1 = new Object[10];
-		for(int i=0;i<10;i++) {
-			temp[i]=new Object();
+		for (int i = 0; i < 10; i++) {
+			temp[i] = new Integer(i);
 		}
 		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < n; j++) 
-				temp1[j] = temp.clone();			
+			for (int j = 0; j < 10; j++)
+				temp1[j] = deepClone(temp);
 			temp = temp1;
 			temp1 = new Object[10];
-			
 		}
-		this.buckets=temp;
-//		this.fill();
+		((Object[]) ((Object[]) ((Object[]) temp[0])[0])[0])[0] = Integer.valueOf(100);
+		this.buckets = temp;
+		this.fill();
+	}
+
+	public Object deepClone(Object[] org) {
+		Object[] clone = new Object[org.length];
+		if (org[0] instanceof Object[]) {
+			for (int i = 0; i < org.length; i++)
+				clone[i] = deepClone((Object[]) org[i]);
+		} else
+			clone = org.clone();
+
+		return clone;
 	}
 
 	private void fill() {
+
 	}
 
+	public static void main(String[] args) {
+		String[] stringarr = { "boo", "bar", "foo", "lol" }; // n=3
+		Indexx idx = new Indexx(stringarr);
+
+		System.out.println((Arrays.deepToString(idx.buckets)));// 3d
+		System.out.println(Arrays.deepToString((Object[]) idx.buckets[0]));// 2d
+		System.out.println(Arrays.deepToString((Object[]) (((Object[]) idx.buckets[0])[0]))); // 1d
+		System.out.println(Arrays.deepToString(((Object[]) (((Object[]) idx.buckets[0])[0])))); // 1d
+	}
 }
