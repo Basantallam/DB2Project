39a4b3fd722cd0b1fed530f95c1e326c78f84596
aya <aya.elgamal14@gmail.com>
2021-06-09 20:11:19 +0200

    Revert "new tests"
    This reverts commit 8ad0a8ef

--- src/test/java/Milestone2Tests.java
+++ src/test/java/Milestone2Tests.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.*;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.stream.Collectors;
+
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -31,35 +31,6 @@ public class Milestone2Tests {
 
     @Test
     @Order(2)
-    public void testDataDirectory() throws Exception {
-        DBApp dbApp = new DBApp();
-        dbApp.init();
-
-        String dataDirPath = "src/main/resources/data";
-        File dataDir = new File(dataDirPath);
-
-        if (!dataDir.isDirectory() || !dataDir.exists()) {
-            throw new Exception("`data` Directory in Resources folder does not exist");
-        }
-
-        ArrayList<String> files = new ArrayList<>();
-        try {
-            files = Files.walk(Paths.get(dataDirPath))
-                    .map(f -> f.toAbsolutePath().toString())
-                    .filter(p -> !Files.isDirectory(Paths.get(p)))
-                    .collect(Collectors.toCollection(ArrayList::new));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//        System.out.println(files);
-        for (String file : files) {
-            Files.delete(Paths.get(file));
-        }
-    }
-
-    @Test
-    @Order(3)
     public void testTableCreation() throws Exception {
         DBApp dbApp = new DBApp();
         dbApp.init();
@@ -73,7 +44,7 @@ public class Milestone2Tests {
     }
 
     @Test
-    @Order(4)
+    @Order(3)
     public void testRecordInsertions() throws Exception {
         DBApp dbApp = new DBApp();
         dbApp.init();
